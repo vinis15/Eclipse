3db2d9f6ccbfbc9d134fc518c827011d2bac26c0
Fabricio Ferreira <61917484+BONEEEE51@users.noreply.github.com>
2020-09-21 02:42:51 -0300

    Removido message do index

--- index.js
+++ index.js
@@ -73,24 +73,6 @@ bot.manager = new Manager({
     });
 
 
-bot.on("message", async message => {
-    if(message.author.bot) return;
-    let prefix;
-    if(!message.guild) prefix = "e."
-    if(message.guild) prefix = config.prefix
-    if(!message.content.startsWith(prefix)) return;
-    if(!message.guild) return message.channel.send("Você não pode usar comandos na DM do bot").catch(e => bot.channels.cache.get("746448706772926554").send(e))
-    let messageArray = message.content.split(' ').join(' ').split(" ");
-    let cmd = messageArray[0]
-    let args = messageArray.slice(1);
-
-    let commandfile = bot.commands.get(cmd.slice(prefix.length));
-    if(!commandfile) commandfile = bot.aliases.get(cmd.slice(prefix.length))
-    if(commandfile) commandfile.run(bot,message,args);
-
-})
-
-
 bot.login(config.token)
 
 module.exports = {
