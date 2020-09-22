3c1d4d6ddb39d6095f88c87d41b8d9390a5badd2
Fabricio Ferreira <61917484+BONEEEE51@users.noreply.github.com>
2020-09-21 21:41:26 -0300

    Agora pode na dm :v:

--- events/message.js
+++ events/message.js
@@ -7,14 +7,13 @@ bot.on("message", async message => {
     if(!message.guild) prefix = "e."
     if(message.guild) prefix = config.prefix
     if(!message.content.startsWith(prefix)) return;
-    if(!message.guild) return message.channel.send("Você não pode usar comandos na DM do bot").catch(e => bot.channels.cache.get("746448706772926554").send(e))
     let messageArray = message.content.split(' ').join(' ').split(" ");
     let cmd = messageArray[0]
     let args = messageArray.slice(1);
 
     let commandfile = bot.commands.get(cmd.slice(prefix.length));
     if(!commandfile) commandfile = bot.aliases.get(cmd.slice(prefix.length))
-    if(!commandfile) commandfile = bot.aliases.get(cmd.slice(prefix.length))
-    if(!commandfile.conf.enabled) return message.channel.send(':x: Comando desativado')
+    if(!commandfile.conf.enabled) return message.channel.send('❌ Comando desativado')
+    if(!message.guild&&commandfile.conf.guildOnly) return message.channel.send("Você não pode usar comandos na DM do bot").catch(e => bot.channels.cache.get("746448706772926554").send(e))
     if(commandfile) commandfile.run(bot,message,args);
 })
