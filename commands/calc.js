9dd338abb40711c4dac4fa13711053aa8d24a1a5
Fabricio Ferreira <61917484+BONEEEE51@users.noreply.github.com>
2020-09-21 22:22:00 -0300

    Shiu

--- commands/calc.js
+++ commands/calc.js
@@ -3,7 +3,7 @@ module.exports.run = async (bot, message, args) => {
 	if (!args.length) return message.reply('insira uma equação.');
 	try {
 		var resultado = happycalculator.calculate(args.join(' '));
-		if (resultado.toString().includes(bot.token)) return message.reply('não foi possível calcular.');
+		if (resultado.toString().includes(bot.token)) return message.reply('TÁ TENTANDO PEGAR MEU TOKEN RAPA?');
 		if (resultado === Infinity) return message.reply('o resultado foi **Infinito**.');
 		return message.reply('`' + resultado + '`');
 	} catch {
