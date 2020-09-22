d2682439e77523a15bc6fd3d209feafc125487aa
Fabricio Ferreira <61917484+BONEEEE51@users.noreply.github.com>
2020-09-22 00:48:38 -0300

    Comando novo OH

---
+++ commands/convite.js
@@ -0,0 +1,21 @@
+const { MessageEmbed, Message } = require("discord.js")
+const config = require("../config.json")
+module.exports.run = async(bot, message, args) => {
+    let embed = new MessageEmbed()
+    embed.setColor(config.color)
+    embed.setAuthor(`${message.author.tag}`, `${message.author.avatarURL({dynamic: true, size: 2048})}`)
+	embed.setDescription(`Olá ${message.author.tag} que bom que gostou das minhas funçoes :V\nEste é o [link de convite](https://discord.com/api/oauth2/authorize?client_id=731185772551733340&permissions=3163136&scope=bot)`)
+    message.channel.send({embed})
+}
+
+exports.conf = {
+    enabled: true,
+    guildOnly: true,
+    aliase: ["invite"]
+}
+exports.help = {
+    nome: "convite",
+    descrição: "Convite do bot",
+    uso: "convite",
+    categoria: "Outros"
+}
