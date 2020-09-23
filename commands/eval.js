ff730797b1da073151ee8ea0affbf812f0bbf921
davipatricio <71164752+davipatricio@users.noreply.github.com>
2020-09-23 01:46:27 -0300

    Update eval.js

--- commands/eval.js
+++ commands/eval.js
@@ -6,15 +6,15 @@ var ids = ["672652538880720896"]
 if (ids.includes (message.author.id)) {
   
         try {
-            if(!args.join(' ')) return message.reply('Esvreve ai')
+            if(!args.join(' ')) return message.reply('Escreve ai')
             let code = await eval(args.join(" "));
-
             if (typeof code !== 'string') code = await require('util').inspect(code, { depth: 0 });
             let embed = new Discord.MessageEmbed()
             .setColor(config.color)
             .addField('📩Entrada', `\`\`\`js\n${args.join(" ")}\`\`\``)
-            .addField('🚩Saída', `\`\`\`js\n${code}\n\`\`\``)
-            message.reply(embed)
+            .addField('🚩Saída', `\`\`\`js\n${code.slice(0, 1010}\n\`\`\``)
+            if(code.length > 1010) embed.addField('🚩 Continuação do Resultado', `\`\`\`js\n${code.slice(1010, 2020}\n\`\`\``)
+            message.reply({embed})
         } catch(e) {
             message.channel.send(`\`\`\`js\n${e}\n\`\`\``);
         }
