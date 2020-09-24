2a80a0beea2b5e518f0f39ec018812e51f593912
davipatricio <71164752+davipatricio@users.noreply.github.com>
2020-09-24 13:53:27 -0300

    Create reload.js

---
+++ commands/Desenvolvedor/reload.js
@@ -0,0 +1,35 @@
+const config = require("../../config.json")
+
+module.exports.run = async (bot, message, args) => {
+
+    if(config.eval.includes(message.author.id) == false) return message.channel.send("Sai irmão")
+
+bot.commands.clear()  
+glob(__dirname+'/../*/*.js', function (er, files) {
+    if(er) { console.log(er); return msg.reply('não foi possível recarregar todos os comandos.'); }
+    files.forEach(f=>{
+        let props = require(`${f.replace('.js', '')}`)
+        bot.commands.set(props.help.nome,props)
+        for (const aliase of props.conf.aliase){
+            bot.aliases.set(aliase,props)
+        };})
+    
+    console.log("[COMANDOS] - Comandos carregados com sucesso")
+})
+
+    message.channel.send(`Comandos recarregados!`)
+
+}
+
+
+exports.conf = {
+    enabled: true,
+    guildOnly: false,
+    aliase: []
+}
+exports.help = {
+    nome: "reload",
+    descrição: "",
+    uso: "reload <COMANDO>",
+    categoria: "Outros"
+}
