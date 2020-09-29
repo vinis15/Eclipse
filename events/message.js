5ab813dd51ef1fd1dcf1caa49cc474950f57762c
davipatricio <71164752+davipatricio@users.noreply.github.com>
2020-09-28 23:43:50 -0300

    Update message.js

--- events/message.js
+++ events/message.js
@@ -12,9 +12,11 @@ bot.on("message", async message => {
     let messageArray = message.content.split(' ').join(' ').split(" ");
     let cmd = messageArray[0]
     let args = messageArray.slice(1);
-
+    let idioma = bot.idiomas.get(message.guild.id) || 'pt'
+    if(idioma==='en') bot.idiomas.en
+    if(idioma==='pt') bot.idiomas.pt
     let commandfile = bot.commands.get(cmd.slice(prefix.length));
     if(!commandfile) commandfile = bot.aliases.get(cmd.slice(prefix.length))
     if(!message.guild&&commandfile.conf.guildOnly) return message.channel.send("Este comando não esta ativado para DM").catch(e => bot.channels.cache.get("746448706772926554").send(e))
-    if(commandfile) commandfile.run(bot,message,args);
+    if(commandfile) commandfile.run(bot,message,args,idioma);
 })
