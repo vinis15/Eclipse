c14c54a3b3aca4b3c417ff29575125eb3166c41b
Fabricio Ferreira <61917484+BONEEEE51@users.noreply.github.com>
2020-10-01 01:57:52 -0300

    a

---
+++ commands/Outros/changelog.js
@@ -0,0 +1,7 @@
+const { MessageEmbed } = require("discord.js")
+const config = require("../../config.json")
+module.exports.run = async (message, bot, args, idioma) => {
+    let embed = new MessageEmbed()
+    embed.setTitle("Changelog")
+    embed.
+}
