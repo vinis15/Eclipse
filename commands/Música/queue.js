16d38ba82259ee03d7297292b17ddece4a282c43
Fabricio Ferreira <61917484+BONEEEE51@users.noreply.github.com>
2020-10-01 00:40:08 -0300

    bugzinho

--- commands/Música/queue.js
+++ commands/Música/queue.js
@@ -24,7 +24,7 @@ module.exports.run = async (bot, message, args, idioma) => {
 
     const maxPages = Math.ceil(queue.length / multiple);
     embed.setColor(config.color)
-    embed.setFooter(`${idioma.queue.arg1.replace(/^./, idioma.queue.arg1[0].toUpperCase()} ${page > maxPages ? maxPages : page} ${idioma.queue.arg3} ${maxPages}`);
+    embed.setFooter(`${idioma.queue.arg1.replace(/^./, idioma.queue.arg1[0].toUpperCase())} ${page > maxPages ? maxPages : page} ${idioma.queue.arg3} ${maxPages}`);
 
     return message.channel.send(embed);
   }
