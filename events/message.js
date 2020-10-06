98a197103fe34400b81019dcc5726613915b48c5
davipatricio <71164752+davipatricio@users.noreply.github.com>
2020-10-06 01:39:12 -0300

    Update message.js

--- events/message.js
+++ events/message.js
@@ -3,9 +3,9 @@ const { bot } = require("../index");
 const ptbr = JSON.parse(JSON.stringify(bot.idiomas.pt))
 const enus = JSON.parse(JSON.stringify(bot.idiomas.en))
 bot.on("message", async message => {
-    prefix()
-    prefix eclipse()
-    function prefix(){
+    prefix(bot, message)
+    prefixeclipse(bot, message)
+    function prefix(bot, message){
     if(message.author.bot) return;
     let prefix;
     if(!message.guild) prefix = "e."
@@ -32,7 +32,7 @@ bot.on("message", async message => {
     if(!message.guild && commandfile.conf.guildOnly) return message.channel.send("Este comando não esta ativado para DM").catch(e => bot.channels.cache.get("746448706772926554").send(e))
     if(commandfile) commandfile.run(bot,message,args,idioma);
 }
-    function prefixeclipse(){
+    function prefixeclipse(bot, message){
     if(message.author.bot) return;
     let messageArray = message.content.split(' ')
     let cmd = messageArray[0]
@@ -49,8 +49,7 @@ bot.on("message", async message => {
     idioma = ptbr
     break;
     }
-    if(message.content.startsWith('<@')&&message.content.endsWith(bot.user.id+'>')) return message.channel.send(`${idioma.message.inico} **${message.author.tag}**, ${idioma.message.meio} \`${config.prefix}\`, ${idioma.message.use} \`${config.prefix}ajuda\` ${idioma.message.ou} \`${config.prefix}help\` ${idioma.message.final} ❤️`)
-    if(!message.content.startsWith('eclipse')||!message.content.startsWith('eclipse ')) return;
+    if(!message.content.toLowerCase().startsWith('eclipse')) return;
     let commandfile = bot.commands.get(cmd.slice('eclipse'.length).trim());
     if(!commandfile) commandfile = bot.aliases.get(cmd.slice('eclipse'.length).toLowerCase().trim())
     if(!message.guild&&commandfile.conf.guildOnly) return message.channel.send("Este comando não esta ativado para DM").catch(e => bot.channels.cache.get("746448706772926554").send(e))
