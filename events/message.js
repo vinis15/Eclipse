65cadca377bc97a635af2a285a57612a177fc889
davipatricio <71164752+davipatricio@users.noreply.github.com>
2020-10-06 02:01:30 -0300

    Update message.js

--- events/message.js
+++ events/message.js
@@ -33,7 +33,8 @@ bot.on("message", async message => {
     if(commandfile) commandfile.run(bot,message,args,idioma);
 }
     function prefixeclipse(bot, message){
-    if(message.author.bot) return;
+if(message.author.bot) return
+if(message.content.toLowerCase().startsWith('eclipse')) {
     let messageArray = message.content.split(' ')
     let cmd = messageArray[0]
     let args = messageArray.slice(1);
@@ -49,10 +50,30 @@ bot.on("message", async message => {
     idioma = ptbr
     break;
     }
-    if(!message.content.toLowerCase().startsWith('eclipse')) return;
     let commandfile = bot.commands.get(cmd.slice('eclipse'.length).trim());
     if(!commandfile) commandfile = bot.aliases.get(cmd.slice('eclipse'.length).toLowerCase().trim())
     if(!message.guild&&commandfile.conf.guildOnly) return message.channel.send("Este comando não esta ativado para DM").catch(e => bot.channels.cache.get("746448706772926554").send(e))
     if(commandfile) commandfile.run(bot,message,args,idioma);
+} else if(message.content.toLowerCase().startsWith('eclipse ')) {
+let messageArray = message.content.split(' ')
+    let cmd = messageArray[0]
+    let args = messageArray.slice(1);
+    let idioma = bot.idioma.get(message.guild.id) || 'pt'
+    switch (idioma.toLowerCase()) {
+    case 'pt':
+    idioma = ptbr
+    break;
+    case 'en':
+    idioma = enus
+    break;
+    default:
+    idioma = ptbr
+    break;
+    }
+    let commandfile = bot.commands.get(cmd.slice('eclipse '.length).trim());
+    if(!commandfile) commandfile = bot.aliases.get(cmd.slice('eclipse '.length).toLowerCase().trim())
+    if(!message.guild&&commandfile.conf.guildOnly) return message.channel.send("Este comando não esta ativado para DM").catch(e => bot.channels.cache.get("746448706772926554").send(e))
+    if(commandfile) commandfile.run(bot,message,args,idioma);
+}
 }
 })
