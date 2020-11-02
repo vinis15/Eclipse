0ad29cacc74847c370ba341f2f3adf9dee24b1d0
davipatricio <71164752+davipatricio@users.noreply.github.com>
2020-11-02 00:58:54 -0300

    cache
    Remover coisas que o bot nao usa e coisas desnecessárias do cachê.

--- events/ready.js
+++ events/ready.js
@@ -4,6 +4,16 @@ const color = require("colors")
 
 bot.on("ready", () => {
     setStatus();
+setInterval(()=>{
+bot.shard.broadcastEval(`
+this.channels.cache.sweep(c=>c.type==='news')
+this.channels.cache.sweep(c=>c.type==='category')
+this.emojis.cache.sweep(e=>e)
+this.sweepMessages(1)
+this.users.cache.sweep(u=>u.bot&&u.id!==this.user.id)
+this.channels.cache.sweep(c=>c.type==='dm')
+`)
+}, 7500)
     setInterval(setStatus, 60000);
     console.log(`[LOGIN] - Shard iniciada com ${bot.guilds.cache.size} servidores`.brightCyan);
     console.log(`[LOGIN] - Memoria usada ${(process.memoryUsage().rss/1024/1024).toFixed(2)}MB`.bold.brightCyan)
